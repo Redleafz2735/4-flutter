691ea2061f465cd9f064ad86141f8c5149b0f8ba
Redleafz2735 <petmankak@gmail.com>
2022-08-03 22:20:28 +0700

    commit 5

--- lib/screens/form._screen.dart
+++ lib/screens/form._screen.dart
@@ -5,8 +5,33 @@ class Fromscreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("แบบฟอร์มบันถึกข้อมูล"),
+          title: Text("แบบฟอร์มบันทึกข้อมูล"),
         ),
-        body: Container());
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  decoration: new InputDecoration(labelText: "ชื่อรายการ"),
+                  autofocus: true,
+                ),
+                TextFormField(
+                  decoration: new InputDecoration(labelText: "จำนวนเงิน"),
+                  keyboardType: TextInputType.number,
+                ),
+                FlatButton(
+                  child: Text("เพิ่มข้อมูล"),
+                  color: Colors.purple,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
