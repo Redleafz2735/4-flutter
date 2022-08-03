59eea1bea978dd0413b1ebc286c42fefd49b27cf
Redleafz2735 <petmankak@gmail.com>
2022-08-04 00:23:30 +0700

    commit 8

---
+++ lib/models/Transaction.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  String title; //ชื่อรายการ
+  double amount; //จำนวนเงิน  
+  DateTime date; //วันที่ เวลา
+
+  Transaction({this,title,this,amount,this,date});
+}
---
+++ lib/providers/transaction_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_application_1/models/Transaction.dart';
+
+class TransactionProvider with ChangeNotifier {
+  // ตัวอย่างข้อมูล
+  List<Transaction> transaction = [
+    Transaction(title: "ซื้อหนังสือ", amount: 500, date: DateTime.now()),
+    Transaction(title: "เสื้อผ้า", amount: 500, date: DateTime.now()),
+    Transaction(title: "กางเกง", amount: 500, date: DateTime.now())
+  ];
+
+  List<Transaction> getTransaction() {
+    return transaction;
+  }
+
+  addTransaction(Transaction statment) {
+    transaction.add(statment);
+  }
+}
