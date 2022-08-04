93affa92c9d755b63532ba18cd0c664d54ae3be8
Redleafz2735 <petmankak@gmail.com>
2022-08-04 23:07:42 +0700

    commit 13

--- lib/main.dart
+++ lib/main.dart
@@ -57,26 +57,36 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: Consumer(
           builder: (context, TransactionProvider provider, child) {
-            return ListView.builder(
-                itemCount: provider.transaction.length,
-                itemBuilder: (context, int index) {
-                  Transaction data = provider.transaction[index];
-                  return Card(
-                    elevation: 10,
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: FittedBox(
-                          child: Text(data.amount.toString()),
+            var count = provider.transaction.length; //นับจำนวนข้อมูล
+            if (count < 0) {
+              return Center(
+                child: Text(
+                  "ไม่พบข้อมูล",
+                  style: TextStyle(fontSize: 30),
+                ),
+              );
+            } else {
+              return ListView.builder(
+                  itemCount: count,
+                  itemBuilder: (context, int index) {
+                    Transaction data = provider.transaction[index];
+                    return Card(
+                      elevation: 10,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 5),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 30,
+                          child: FittedBox(
+                            child: Text(data.amount.toString()),
+                          ),
                         ),
+                        title: Text(data.title),
+                        subtitle: Text(data.date.toString()),
                       ),
-                      title: Text(data.title),
-                      subtitle: Text(data.date.toString()),
-                    ),
-                  );
-                });
+                    );
+                  });
+            }
           },
         ));
   }
