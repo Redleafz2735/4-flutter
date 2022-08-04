f3c275238d09cdaa262928e976f03015d1b070d0
Redleafz2735 <petmankak@gmail.com>
2022-08-04 22:33:44 +0700

    commit 11

--- lib/screens/form._screen.dart
+++ lib/screens/form._screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Fromscreen extends StatelessWidget {
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,23 +11,42 @@ class Fromscreen extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Form(
+            key: formkey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TextFormField(
                   decoration: new InputDecoration(labelText: "ชื่อรายการ"),
                   autofocus: true,
+                  validator: (var str) {
+                    //ชือ่รายการเป็นค่่าว่าง
+                    if (str!.isEmpty) {
+                      return "กรุณาป้อนชื่อรายการ";
+                    }
+                    return null;
+                  },
                 ),
                 TextFormField(
-                  decoration: new InputDecoration(labelText: "จำนวนเงิน"),
-                  keyboardType: TextInputType.number,
-                ),
+                    decoration: new InputDecoration(labelText: "จำนวนเงิน"),
+                    keyboardType: TextInputType.number,
+                    validator: (var str) {
+                      //ชือ่รายการเป็นค่่าว่าง
+                      if (str!.isEmpty) {
+                        return "กรุณาป้อนจำนวนเงิน";
+                      }
+                      if (double.parse(str) <= 0) {
+                        return "กรุณาระบุจำนวนเงินมากกว่า 0";
+                      }
+                      return null;
+                    }),
                 FlatButton(
                   child: Text("เพิ่มข้อมูล"),
                   color: Colors.purple,
                   textColor: Colors.white,
                   onPressed: () {
-                    Navigator.pop(context);
+                    if (formkey.currentState!.validate()) {
+                      Navigator.pop(context);
+                    }
                   },
                 )
               ],
